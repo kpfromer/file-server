1f717d7b99fff3d3412f5af89f4d23533a9a1878
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-26 14:28:14 -0600

    Update express app to use react and api

--- express-http/src/app.js
+++ express-http/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import path from 'path';
-import multer from 'multer';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// Routers
+import api from './api';
+import react from './react';
 
 const app = express();
 const port = 3000;
@@ -12,41 +15,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cors())
 
-const multerConfig = {
-  storage: multer.diskStorage({
-    //Setup where the user's file will go
-    destination: (req, file, next) => {
-      next(null, './');
-    },
-
-    //Then give the file a unique name
-    filename: (req, file, next) => {
-      console.log(file);
-      const ext = file.mimetype.split('/')[1];
-      next(null, file.fieldname + '-' + Date.now() + '.' + ext);
-    }
-  }),
-
-  //A means of ensuring only images are uploaded. 
-  fileFilter: (req, file, next) => {
-    if (!file) {
-      next();
-    }
-    const image = file.mimetype.startsWith('image/');
-    if (image) {
-      console.log('photo uploaded');
-      next(null, true);
-    } else {
-      console.log("file not supported");
-
-      //TODO:  A better message response to user on failure.
-      return next();
-    }
-  }
-};
-
-app.post('/upload', multer(multerConfig).single('photo'), (req, res) => {
-  res.send('Complete!');
-});
+app.use('/api', api);
+app.use(react);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
