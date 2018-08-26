f2de150521449acba98d68fe354ac2e74ae1db6b
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-26 15:58:42 -0600

    Refactor gulpfile

--- express-http/gulpfile.js
+++ express-http/gulpfile.js
@@ -9,16 +9,15 @@ const Cache = require('gulp-file-cache');
 const cache = new Cache();
 
 gulp.task('compile', function () {
-  const stream = gulp.src('./src/**/*.js') // your ES2015 code
+  return gulp.src('./src/**/*.js') // your ES2015 code
     .pipe(cache.filter()) // remember files
     .pipe(babel({ presets: ['env'] })) // compile new ones
     .pipe(cache.cache()) // cache them
     .pipe(gulp.dest('./dist')) // write them
-  return stream; // important for gulp-nodemon to wait for completion
 });
 
 gulp.task('watch', ['compile'], function () {
-  var stream = nodemon({
+  const stream = nodemon({
     script: 'dist/app.js', // run ES5 code
     watch: 'src', // watch ES2015 code
     tasks: ['compile'] // compile synchronously onChange
@@ -33,7 +32,6 @@ gulp.task('build', ['clean'], () => {
   return gulp.src('./src/**/*.js')
     .pipe(concat('index.js'))
     .pipe(babel({ presets: ['env'] }))
-    // .pipe(rename('scripts.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build'));
 });
